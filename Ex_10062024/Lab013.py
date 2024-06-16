1c4ba6e3429c6fad8c2a6d7b4532cffa4757f34a
Veeresh <veereshmtl@gmail.com>
2024-06-16 23:22:42 +0530

    Literals

---
+++ Ex_10062024/Lab013.py
@@ -0,0 +1,15 @@
+#Literals
+# var_name = variable value
+name = "Veeresh"
+#var_name --> identifier
+#var_val --> literal
+# Literals are actual identifier
+# Literals can be Numeric or Non-Numeric
+
+age = 12
+pi = 13.4
+is_married = True
+have_limbo = None
+my_list = []
+my_set = {1,2,3,4,5,5,6} # Set is a collection of unique or non-duplicate items
+print(my_set)
