9b624448fe2c85ea180a08ae7574a6c8c1604073
Veeresh <veereshmtl@gmail.com>
2024-06-16 23:53:35 +0530

    Literals

---
+++ Ex_10062024/Lab014.py
@@ -0,0 +1,11 @@
+# Ternary Operator
+
+veeresh_marks = 90
+ravi_marks = 95
+
+print("Veeresh Passed" if veeresh_marks >= 85 else "Veeresh Failed")
+
+if veeresh_marks > ravi_marks:
+    print("Veeresh Passed")
+else:
+    print("Raviesh Passed")
---
+++ Ex_10062024/Lab015.py
@@ -0,0 +1,11 @@
+# Write a program to calculate the area of the circle
+# input --> radius
+# output --> area of the circle
+# data types
+# input -> int or float -> float
+# output --> float
+
+import math
+
+
+radius = float(input("Enter the radius of the circle: "))
